fcc2c6c05ccc0a2fd3f8c0391c98b97727d58991
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-01-25 13:32:10 -0500

    Level4

--- JS/levels.js
+++ JS/levels.js
@@ -9,12 +9,41 @@ levels=[
 			{position:{x:450,y:540},type:1,color:0,level:0,width:150,height:60},
 			{position:{x:600,y:510},type:1,color:0,level:0,width:150,height:90},
 		],
+	},{
+		edge:{x:900,y:600},
+		player:[510,510],
+		walls:[
+			{position:{x:0,y:540},type:1,color:0,level:0,width:90,height:60},
+			{position:{x:420,y:540},type:1,color:0,level:0,width:60,height:60},
+			{position:{x:810,y:540},type:1,color:0,level:0,width:90,height:60},
+		],
+	},{
+		edge:{x:1050,y:600},
+		player:[510,510],
+		walls:[
+			{position:{x:0,y:540},type:1,color:0,level:0,width:120,height:60},
+			{position:{x:930,y:540},type:1,color:0,level:0,width:120,height:60},
+			{position:{x:510,y:480},type:1,color:0,level:0,width:30,height:30},
+			{position:{x:240,y:420},type:1,color:0,level:0,width:30,height:30},
+			{position:{x:780,y:420},type:1,color:0,level:0,width:30,height:30},
+		],
+	},{
+		edge:{x:1200,y:600},
+		player:[510,210],
+		walls:[
+			{position:{x:0,y:540},type:1,color:0,level:0,width:90,height:60},
+			{position:{x:450,y:540},type:1,color:0,level:0,width:90,height:60},
+			{position:{x:1140,y:240},type:1,color:0,level:0,width:60,height:360},
+			{position:{x:300,y:0},type:1,color:0,level:0,width:30,height:300},
+			{position:{x:330,y:270},type:1,color:0,level:0,width:30,height:30},
+			{position:{x:660,y:210},type:1,color:0,level:0,width:30,height:30},
+		],
 	},
 	/*{
 		edge:{x:0,y:600},
 		player:[0,0],
 		walls:[
-			{position:{x:0,y:0},type:1,level:0,width:0,height:0},
+			{position:{x:0,y:0},type:1,color:0,level:0,width:0,height:0},
 		],
 	},*/
 ]
