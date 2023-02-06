d7517d681087895c607b5f2702533683342d546a
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-02-05 22:16:57 -0500

    Level22

--- JS/levels.js
+++ JS/levels.js
@@ -264,7 +264,7 @@ levels=[
 		],
 	},{
 		edge:{x:600,y:1800},
-		player:[1710,1710],
+		player:[1710,210],
 		walls:[
 			{position:{x:0,y:1740},type:1,color:0,level:0,width:90,height:60},
 			{position:{x:510,y:240},type:1,color:0,level:0,width:90,height:60},
@@ -286,6 +286,35 @@ levels=[
 			{position:{x:0,y:1020},type:6,color:1,level:0,width:570,height:30},
 			{position:{x:0,y:780},type:6,color:3,level:0,width:570,height:30},
 			{position:{x:0,y:540},type:6,color:1,level:0,width:570,height:30},
+		],
+	},{
+		edge:{x:600,y:600},
+		player:[510,510],
+		walls:[
+			{position:{x:0,y:540},type:1,color:0,level:0,width:600,height:60},
+			{position:{x:150,y:510},type:2,color:1,level:0,width:30,height:30},
+			{position:{x:300,y:510},type:2,color:2,level:0,width:30,height:30},
+			{position:{x:300,y:270},type:2,color:3,level:0,width:30,height:30},
+			{position:{x:210,y:0},type:1,color:2,level:0,width:60,height:540},
+			{position:{x:360,y:0},type:1,color:2,level:0,width:60,height:540},
+			{position:{x:365,y:0},type:1,color:1,level:0,width:50,height:540},
+		],
+	},{
+		edge:{x:750,y:600},
+		player:[510,510],
+		walls:[
+			{position:{x:0,y:540},type:1,color:0,level:0,width:90,height:60},
+			{position:{x:660,y:540},type:1,color:0,level:0,width:90,height:60},
+			{position:{x:300,y:240},type:1,color:1,level:0,width:150,height:90},
+			{position:{x:300,y:330},type:1,color:3,level:0,width:150,height:90},
+			{position:{x:300,y:420},type:1,color:2,level:0,width:150,height:90},
+			{position:{x:360,y:270},type:2,color:2,level:0,width:30,height:30},
+			{position:{x:360,y:360},type:2,color:1,level:0,width:30,height:30},
+			{position:{x:360,y:450},type:2,color:3,level:0,width:30,height:30},
+			{position:{x:60,y:450},type:2,color:3,level:0,width:30,height:30},
+
+			{position:{x:365,y:320},type:4,color:0,level:0,width:20,height:20},
+			{position:{x:365,y:410},type:4,color:0,level:0,width:20,height:20},
 		],
 	},
 	/*{
