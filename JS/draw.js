fc26ff0ff22d66a43be02835c02f06b8f0bb7e04
DuckyProgramming <93790036+DuckyProgramming@users.noreply.github.com>
2023-02-06 22:26:06 -0500

    Packaging

--- JS/draw.js
+++ JS/draw.js
@@ -2,9 +2,9 @@ function draw(){
 	clear()
 	background(125)
 	graphics.main.clear()
-	graphics.main.push()
 	switch(stage.scene){
 		case 'level':
+			graphics.main.push()
 			if(game.color>0){
 				graphics.main.background(mapColor([360,360,360],game.color)[0],mapColor([360,360,360],game.color)[1],mapColor([360,360,360],game.color)[2])
 			}else{
@@ -31,9 +31,56 @@ function draw(){
 				}
 			}
 			displayBorder(graphics.main,game.edge)
+			graphics.main.pop()
+			graphics.main.noStroke()
+			graphics.main.fill(100)
+			graphics.main.textSize(20)
+			graphics.main.text(ceil(game.time/6)/10,graphics.main.width/2,40)
+			graphics.main.textSize(15)
+			for(let a=0,la=min(game.splits.length,20);a<la;a++){
+				graphics.main.text(game.splits[a],50,15+a*15)
+			}
+			for(let a=20,la=min(game.splits.length,40);a<la;a++){
+				graphics.main.text(game.splits[a],150,15+(a-20)*15)
+			}
+			if(game.maxZone<game.zone){
+				game.maxZone++
+				game.splits.push((game.zone)+": "+ceil(game.time/6)/10)
+			}
+			game.time++
+		break
+		case 'menu':
+			graphics.main.background(240)
+			graphics.main.noStroke()
+			graphics.main.fill(120)
+			graphics.main.textSize(90)
+			graphics.main.text("RGBBox",graphics.main.width/2,graphics.main.height/2-75)
+			graphics.main.textSize(30)
+			graphics.main.text("WASD or Arrow Keys to Move\n\nClick to Begin",graphics.main.width/2,graphics.main.height/2+60)
+		break
+		case 'end':
+			graphics.main.background(0)
+			graphics.main.noStroke()
+			graphics.main.fill(255)
+			graphics.main.noStroke()
+			graphics.main.textSize(25)
+			graphics.main.text("RGBBox\nUse this screen\nfor record proof",graphics.main.width/2,graphics.main.height/2)
+			graphics.main.fill(100)
+			graphics.main.textSize(20)
+			graphics.main.text(ceil(game.time/6)/10,graphics.main.width/2,40)
+			graphics.main.textSize(15)
+			for(let a=0,la=min(game.splits.length,20);a<la;a++){
+				graphics.main.text(game.splits[a],50,15+a*15)
+			}
+			for(let a=20,la=min(game.splits.length,40);a<la;a++){
+				graphics.main.text(game.splits[a],150,15+(a-20)*15)
+			}
+			if(game.maxZone<game.zone){
+				game.maxZone++
+				game.splits.push((game.zone)+": "+ceil(game.time/6)/10)
+			}
 		break
 	}
-	graphics.main.pop()
 	stage.scale=min(width/graphics.main.width,height/graphics.main.height)
 	displayTransition(graphics.main,transition)
 	image(graphics.main,width/2-stage.scale*graphics.main.width/2,height/2-stage.scale*graphics.main.height/2,stage.scale*graphics.main.width,stage.scale*graphics.main.height)
--- JS/mouse.js
+++ JS/mouse.js
@@ -1,5 +1,9 @@
 function mouseClicked(){
 	updateMouse(graphics.main)
 	switch(stage.scene){
+		case 'menu':
+			transition.trigger=true
+			transition.scene='level'
+		break
 	}
 }
